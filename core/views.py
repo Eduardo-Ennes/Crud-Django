f5c03156475e5c5ae4baea5b5b7d23e5505176a7
Eduardo Ennes <eduardosimoes9899@gmail.com>
2025-05-28 19:15:48 -0300

    implementacao da rota get do projeto

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView 
+from .models import Location
 
-# Create your views here.
+class Crud_Location(APIView):
+    def get(self, request):
+        data = Location.objects.all()
+        context = {'datas': data}
+        return render(request, 'home.html', context)
+    
+    def post(self, request):
+        ...
+        
+    def put(self, request):
+        ...
+        
+    def delete(self, request):
+        ...
