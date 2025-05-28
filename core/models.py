3e916e6ebb5e47dff1581a2688406a6dac2e5d81
Eduardo Ennes <eduardosimoes9899@gmail.com>
2025-05-28 18:15:03 -0300

    configuracoes do model do projeto

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Mesmo que as tabelas já estejam criadas no mysql, para haver interação com o db deve-se especificar as tabelas e seus valores na classe
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)  # Data de criação
+    update = models.DateTimeField(auto_now=True)  # Última atualização
+    
+    class Meta:
+        db_table = 'countries'
+        managed = False  # Informa ao Django que não deve tentar gerenciar a tabela
